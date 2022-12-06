28a1684f0a3f06d7d1a39612fdcef84e1720738c
Tran Ho <tranho8596@gmail.com>
2022-12-06 11:56:36 -0800

    added new files

---
+++ question/app.js
@@ -0,0 +1,30 @@
+//using selectors inside the element
+
+/*
+const questions = document.querySelectorAll(".question");
+questions.forEach(function(question){
+const btn = question.querySelector(".question-btn");
+btn.addEventListener("click", function(){
+    questions.forEach(function(item){
+        if(item !== question){
+            item.classList.remove("show-text");
+        }
+    })
+    question.classList.toggle("show-text");
+
+})
+})
+*/
+// traversing the dom
+
+const btns = document.querySelectorAll(".question-btn");
+btns.forEach(function(btn){
+    btn.addEventListener("click", function(e){
+        btns.forEach(function(item){
+            if(item !== btn){
+                item.parentElement.parentElement.classList.remove("show-text");
+            }
+        })
+        btn.parentElement.parentElement.classList.toggle("show-text");
+    })
+})
